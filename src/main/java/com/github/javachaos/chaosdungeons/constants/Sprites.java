88280eb03c279a188ce33ac1e10e93e0c4f93ba4
Javachaos <18709177+javachaos@users.noreply.github.com>
2023-08-02 15:16:29 -0400

    Added a few things, cleaned up a few things.

--- src/main/java/com/github/javachaos/chaosdungeons/constants/Sprites.java
+++ src/main/java/com/github/javachaos/chaosdungeons/constants/Sprites.java
@@ -1,10 +1,13 @@
 package com.github.javachaos.chaosdungeons.constants;
 
+/**
+ * Sprite String constants class.
+ */
 public class Sprites {
-    public static final String PLAYER = "player";
-    public static final String FIREBALL = "fireball";
+  public static final String PLAYER = "player";
+  public static final String FIREBALL = "fireball";
 
-    private Sprites() {
-        //unused
-    }
+  private Sprites() {
+    //unused
+  }
 }
--- src/test/java/com/github/javachaos/chaosdungeons/geometry/utils/ImageTestUtils.java
+++ src/test/java/com/github/javachaos/chaosdungeons/geometry/utils/ImageTestUtils.java
@@ -1,58 +1,68 @@
 package com.github.javachaos.chaosdungeons.geometry.utils;
 
 import com.github.javachaos.chaosdungeons.geometry.polygons.Vertex;
-
-import javax.imageio.ImageIO;
-import javax.swing.*;
-import java.awt.*;
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.Graphics2D;
 import java.awt.geom.Point2D;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import java.util.List;
+import javax.imageio.ImageIO;
+import javax.swing.JPanel;
 
+/**
+ * Image Test utility class for drawing a polygon to a png image.
+ */
 public class ImageTestUtils {
-    public static void drawPolygon(Vertex poly, String filename) {
-        List<Point2D> p = poly.getPoints();
-        int WIDTH = 512;
-        int HEIGHT = 512;
-        final BufferedImage img = new BufferedImage(WIDTH, HEIGHT,
-                BufferedImage.TYPE_INT_ARGB);
-        Graphics2D g2d = (Graphics2D) img.getGraphics();
-        g2d.setBackground(Color.WHITE);
-        g2d.setColor(Color.WHITE);
-        g2d.setPaint(Color.RED);
-        g2d.setStroke(new BasicStroke(2.0f));
-        drawShape(g2d, p);
-        JPanel jp = new JPanel();
-        jp.printAll(g2d);
-        jp.setVisible(true);
-        writeBufferedImageToFile(img,
-                new File(filename));
-    }
 
-    private static void writeBufferedImageToFile(BufferedImage img, File f) {
-        try {
-            ImageIO.write(img, "PNG", f);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
+  /**
+   * Draw a polygon to a png file at filename.
+   *
+   * @param poly the polygon as a vertex
+   * @param filename the file to save the PNG to
+   */
+  public static void drawPolygon(Vertex poly, String filename) {
+    int w = 512;
+    int h = 512;
+    final BufferedImage img = new BufferedImage(w, h,
+        BufferedImage.TYPE_INT_ARGB);
+    Graphics2D g2d = (Graphics2D) img.getGraphics();
+    g2d.setBackground(Color.WHITE);
+    g2d.setColor(Color.WHITE);
+    g2d.setPaint(Color.RED);
+    g2d.setStroke(new BasicStroke(2.0f));
+    drawShape(g2d, poly.getPoints());
+    JPanel jp = new JPanel();
+    jp.printAll(g2d);
+    jp.setVisible(true);
+    writeBufferedImageToFile(img,
+        new File(filename));
+  }
 
-    private static void drawShape(Graphics2D g2d, List<Point2D> points) {
-        int[] xPoints = new int[points.size()];
-        int[] yPoints = new int[points.size()];
-        int i = 0;
-        for (Point2D point : points) {
-            xPoints[i] = (int) point.getX();
-            yPoints[i] = (int) point.getY();
-            i++;
-        }
-        g2d.drawPolygon(xPoints, yPoints, points.size());
-        for (i = 0; i < points.size(); i++) {
-            Point2D point = points.get(i);
-            String coordinatesText = "P[ " + i + ": x = " + point.getX() + ", y = " + point.getY() + " ]";
-            g2d.drawString(coordinatesText, (int) point.getX(), (int) point.getY());
-        }
+  private static void writeBufferedImageToFile(BufferedImage img, File f) {
+    try {
+      ImageIO.write(img, "PNG", f);
+    } catch (IOException e) {
+      throw new RuntimeException(e);
     }
+  }
+
+  private static void drawShape(Graphics2D g2d, List<Point2D> points) {
+    int[] x = new int[points.size()];
+    int[] y = new int[points.size()];
+    int i = 0;
+    for (Point2D point : points) {
+      x[i] = (int) point.getX();
+      y[i] = (int) point.getY();
+      i++;
+    }
+    g2d.drawPolygon(x, y, points.size());
+    for (i = 0; i < points.size(); i++) {
+      Point2D point = points.get(i);
+      String coordinatesText = "P[ " + i + ": x = " + point.getX() + ", y = " + point.getY() + " ]";
+      g2d.drawString(coordinatesText, (int) point.getX(), (int) point.getY());
+    }
+  }
 }
