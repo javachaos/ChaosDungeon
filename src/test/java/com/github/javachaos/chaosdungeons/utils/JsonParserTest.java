49c5a14fa915ac3fac9ca79fb248c477bc42988d
Alfred Laderoute <alfred_e@outlook.com>
2024-02-19 18:49:49 -0500

    Lots of changes.

--- src/test/java/com/github/javachaos/chaosdungeons/utils/JsonParserTest.java
+++ src/test/java/com/github/javachaos/chaosdungeons/utils/JsonParserTest.java
@@ -17,7 +17,6 @@ class JsonParserTest {
         Map<String, Object> data = jp.parse();
         ArrayList<Object> json = (ArrayList<Object>) data.get("menu");
         LOGGER.debug(json);
-        //assertEquals("[{title=[example glossary], GlossDiv=[{GlossList=[{GlossEntry=[{GlossTerm=[Standard Generalized Markup Language], SortAs=[SGML], GlossDef=[{para=[A meta-markup language, used to create markup languages such as DocBook.], GlossSeeAlso=[[[GML], [XML]]]}], ID=[SGML], Acronym=[SGML], Abbrev=[ISO 8879:1986]}]}], title=[S]}]}]", json.toString());
     }
 
 }
